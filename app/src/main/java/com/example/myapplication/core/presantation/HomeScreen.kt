340615b3f05b9f13455186650e0ae7eec77807d0
Surender Kumar <surenderkumar0011996@gmail.com>
2026-03-28 16:11:01 +0530

    add text

--- app/src/main/java/com/example/myapplication/core/presantation/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/core/presantation/HomeScreen.kt
@@ -1,9 +1,15 @@
 package com.example.myapplication.core.presantation
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeScreen() {
+
+    Scaffold { iner->
+        Text("hello")
+    }
 
 }
