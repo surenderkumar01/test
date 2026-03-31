d45a99a19929f3ba8c640f72294c4162cec59046
Surender Kumar <surenderkumar0011996@gmail.com>
2026-03-31 09:29:35 +0530

    mgs

--- app/src/main/java/com/example/myapplication/core/data/Contect.kt
+++ app/src/main/java/com/example/myapplication/core/data/Contect.kt
@@ -2,5 +2,6 @@ package com.example.myapplication.core.data
 
 data class Contect(
     val name:String?=null,
-    val contectNumber:String?=null
+    val contectNumber:String?=null,
+    val read:String?=null
 )
